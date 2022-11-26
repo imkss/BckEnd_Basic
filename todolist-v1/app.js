e01055f0e294241f4b55ac90116530f61b0cb860
imkss <ksb020@nist.edu>
2022-11-26 17:42:44 +0530

    Added new Page

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -9,25 +9,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-var items = ["Eat, Sleep, Code & Repeat"];
+let items = ["Eat, Sleep, Code & Repeat"];
+let workItems = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
+  let today = new Date();
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 app.listen(4000, function () {
